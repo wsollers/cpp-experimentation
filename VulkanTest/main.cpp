a483fbc32a8b58e2496a1368338f07541cce0167
BoondockTaints <wsollers@gmail.com>
2024-01-17 11:08:12 -0500

    debugging hooked up
    debugging hooked up

--- VulkanTest/main.cpp
+++ VulkanTest/main.cpp
@@ -3,34 +3,33 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
-
+#include <glm/vec4.hpp>
 #include <iostream>
 
-int main()
-{
-    glfwInit();
+int main() {
+  glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow* window =
+      glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
+  std::cout << extensionCount << " extensions supported\n";
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
 
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
 
-    glfwDestroyWindow(window);
+  glfwDestroyWindow(window);
 
-    glfwTerminate();
+  glfwTerminate();
 
-    return 0;
+  return 0;
 }
